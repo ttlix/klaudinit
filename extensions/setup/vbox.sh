8956173a73850d681b0915a227c891f664c00d63
Titanlix Io <titanlixio@gmail.com>
2018-08-30 07:38:01 +0000

    Use loop/with_items keywords
    - fixed vols_part / swap_part actions
    - fixed group_vars settings

--- extensions/setup/vbox.sh
+++ extensions/setup/vbox.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 MAIN_LINK=https://github.com/titanlixio/klaudinit.git
 MAIN_IFDEV=enp0s3
 MAIN_KLAUDENV=dev
@@ -12,15 +14,14 @@ yum install -y openssh-server openssh-clients git
 systemctl start sshd
 cd /tmp
 git clone $MAIN_LINK
-###-----------------------------------------------###
-###-----------------------------------------------###
+###-------------------------------------------------###
+###-------------------------------------------------###
 KLAUDENV=dev \
   sh /tmp/klaudinit/extensions/setup/main.sh      \\
   -l localhost                                    \\
-  -e NODE_NAME=node01 -e NODE_DOMAIN=dev.ttlix    \\
-  -e VOLS_PART=True   -e VOL1_NAME=/dev/sdb       \\ 
-  -e SWAP_PART=True   -e SWAP_PATH=/home/swapfile
-###-----------------------------------------------###
-###-----------------------------------------------###
+  -e node_name=node01 -e node_domain=dev.ttlix    \\
+  -e vols_part=False  -e swap_part=False
+###-------------------------------------------------###
+###-------------------------------------------------###
 
 EOF
